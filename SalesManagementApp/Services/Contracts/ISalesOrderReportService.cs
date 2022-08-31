8c21275224cc93fcdfe7ee7890883cb8cf822e21
debasispaul <idebasispaul@gmail.com>
2022-08-30 23:23:07 -0700

    ProjectUpdated

--- SalesManagementApp/Services/Contracts/ISalesOrderReportService.cs
+++ SalesManagementApp/Services/Contracts/ISalesOrderReportService.cs
@@ -4,8 +4,15 @@ namespace SalesManagementApp.Services.Contracts
 {
     public interface ISalesOrderReportService
     {
+        //SR
         Task<List<GroupedFieldPriceModel>> GetEmployeePricePerMonthData();
         Task<List<GroupedFieldQtyModel>> GetQtyPerProductCategory();
         Task<List<GroupedFieldQtyModel>> GetQtyPerMonthData();
+
+        //TL
+        Task<List<GroupedFieldPriceModel>> GetGrossPerTeamMemberData();
+        Task<List<GroupedFieldQtyModel>> GetQtyPerTeamMemberData();
+        Task<List<GroupedFieldQtyModel>> GetTeamQtyPerMonthData();
+
     }
 }
--- SalesManagementApp/Services/SalesOrderReportService.cs
+++ SalesManagementApp/Services/SalesOrderReportService.cs
@@ -13,6 +13,8 @@ namespace SalesManagementApp.Services
         {
             this.salesManagementDbContext = salesManagementDbContext;
         }
+
+        //SR
         public async Task<List<GroupedFieldPriceModel>> GetEmployeePricePerMonthData()
         {
             try
@@ -49,6 +51,7 @@ namespace SalesManagementApp.Services
                 throw;
             }
         }
+
 
         public async Task<List<GroupedFieldQtyModel>> GetQtyPerMonthData()
         {
@@ -106,6 +109,102 @@ namespace SalesManagementApp.Services
 
                 throw;
             }
+        }
+
+        //TL
+        public async Task<List<GroupedFieldPriceModel>> GetGrossPerTeamMemberData()
+        {
+            try
+            {
+                List<int> teamMemberIds = await GetTeamMemberIds(3);
+
+                var reportData = await (from s in this.salesManagementDbContext.SalesOrderReports
+                                        where teamMemberIds.Contains(s.EmployeeId)
+                                        group s by s.EmployeeFirstName into GroupedData
+                                        orderby GroupedData.Key
+                                        select new GroupedFieldPriceModel
+                                        {
+                                            GroupedFieldKey = GroupedData.Key,
+                                            Price = Math.Round(GroupedData.Sum(oi => oi.OrderItemPrice), 2)
+                                        }).ToListAsync();
+                return reportData;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<GroupedFieldQtyModel>> GetQtyPerTeamMemberData()
+        {
+            try
+            {
+                List<int> teamMemberIds = await GetTeamMemberIds(3);
+                var reportData = await (from s in this.salesManagementDbContext.SalesOrderReports
+                                       where teamMemberIds.Contains(s.EmployeeId)
+                                       group s by s.EmployeeFirstName into GroupedData
+                                       orderby GroupedData.Key
+                                       select new GroupedFieldQtyModel
+                                       {
+                                           GroupedFieldKey = GroupedData.Key,
+                                           Qty = GroupedData.Sum(oi => oi.OrderItemQty)
+                                       }).ToListAsync();
+                return reportData;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<GroupedFieldQtyModel>> GetTeamQtyPerMonthData()
+        {
+            try
+            {
+                List<int> teamMemberIds = await GetTeamMemberIds(3);
+
+                var reportData = await (from s in this.salesManagementDbContext.SalesOrderReports
+                                        where teamMemberIds.Contains(s.EmployeeId)
+                                        group s by s.OrderDateTime.Month into GroupedData
+                                       orderby GroupedData.Key
+                                       select new GroupedFieldQtyModel
+                                       {
+                                           GroupedFieldKey = (
+                                           GroupedData.Key == 1 ? "Jan" :
+                                           GroupedData.Key == 2 ? "Feb" :
+                                           GroupedData.Key == 3 ? "Mar" :
+                                           GroupedData.Key == 4 ? "Apr" :
+                                           GroupedData.Key == 5 ? "May" :
+                                           GroupedData.Key == 6 ? "Jun" :
+                                           GroupedData.Key == 7 ? "Jul" :
+                                           GroupedData.Key == 8 ? "Aug" :
+                                           GroupedData.Key == 9 ? "Sep" :
+                                           GroupedData.Key == 10 ? "Oct" :
+                                           GroupedData.Key == 11 ? "Nov" :
+                                           GroupedData.Key == 12 ? "Dec" :
+                                           ""
+
+                                           ),
+                                           Qty = GroupedData.Sum(o => o.OrderItemQty)
+                                       }).ToListAsync();
+                return reportData;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private async Task<List<int>> GetTeamMemberIds(int teamLeadId)
+        {
+            List<int> teamMemberIds = await this.salesManagementDbContext.Employees
+                                        .Where(e => e.ReportToEmpId == teamLeadId)
+                                        .Select(e => e.Id).ToListAsync();
+            return teamMemberIds;
         }
 
     }
